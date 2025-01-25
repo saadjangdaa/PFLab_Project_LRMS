577590fd604eceee313bd83ad6a3f9c8f7a4f091
Uqbah Bilal Learner's <153458632+Optimisticity@users.noreply.github.com>
2025-01-26 03:19:47 +0500

    Add files via upload
    Adding and Displaying done

---
+++ Inventory_Uqbah/laptop-inventory-management.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+// #include "functions.h"
+
+// // functions initialization
+void keyTrackersa();
+void addLaptop();
+void displayLaptop();
+// void updateLaptopStatus();
+
+
+void main(){
+    keyTracker();
+    // addLaptop();
+}
+
+void keyTracker(){
+    int key;
+    printf("Here are the Key-Bindings for navigation:-\nAdd Laptop\t[1]\nDisplay Laptops\t[2]\n");
+    printf("Press any key of your choice: ");
+    scanf("%d", &key);
+    
+    if (key == 1){
+        addLaptop();
+    } else if (key == 2){
+        displayLaptop();
+    }
+}
+
+void addLaptop(){
+    FILE *fptr = fopen("Laptop-Data.txt", "a");
+    char input = 0;
+    fflush(stdin);
+    printf("Enter the laptop data: ");  
+    while ((input = getchar())!= '\n'){
+        putc(input, fptr);
+    }
+    fprintf(fptr, "\n\n");
+    fclose(fptr);
+}   
+void displayLaptop(){
+    char read;
+    FILE *fptr = fopen("Laptop-Data.txt", "r");
+    if (fptr == NULL){
+        printf("File does not exist");
+    } else {
+        while((read = getc(fptr))!=EOF){
+            printf("%c", read);
+        }
+    }
+    fclose(fptr);
+}
